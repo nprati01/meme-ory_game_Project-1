84c1f8bc851b883e8411500c5f1ebb071a45e8a7
nprati01 <nicole.e.prati@gmail.com>
2023-03-12 18:37:16 -0400

    mvp, styling and readMe additions

---
+++ README.md
@@ -0,0 +1,104 @@
+Game Title: How good is your memory?
+
+Game Description:
+The game will be composed of 16 cards, there will be 8 pairs.
+The player will have 10 guesses ( 20 total clicks)
+If the player successfully matches all 8 pairs in 10 guesses they will win otherwise they lose
+If the player makes a match the cards remain flipped face side  up, if they do not match the cards will flip back over
+
+Game play:
+See Game Play folder in Assets
+p1_Memory_Game/assets/game play
+
+Technologies used:
+JavaScript, HTML, CSS, Adobe Illustrator
+
+Getting Started
+https://nprati01.github.io/meme-ory_game_Project-1/
+
+
+Next Steps:
+Future Enhancemnets:
+Cards will have a flip animation
+Cards will have a shuffle animation
+The board will be hidden until user clicks play game button
+When the game is over the board will be hidden, the win/lose message will display, and the shuffle cards and play again button will display
+Clean up code, so all code isnt within the click event listener 
+
+
+
+
+
+
+
+
+
+
+
+
+
+Pseudo code
+1) Define required Constants:
+    1.1)Define a faceArray object that contains the images for each cars
+
+
+2 Define Required variables used to track the state of the game
+2.1 a matches variable-a variable to determine the number of matches
+  let matches
+2.2 a guess variable will count the number of guesses a player has
+  let guessesLeft
+2.3 a variable that will reference the cards-represents an array of cards to display when game starts
+let cards = []
+2.4 a variable that will store first click
+let firstClick = ''
+2.5 a variable that will store second click
+let secondClick = ''
+2.6 Variables to store each click event
+let click1
+let click2
+
+
+
+3) Store elements on the page that will be accessed in the code more than once
+    3.1)
+
+4) Upon loading the app should:
+    4.1)Initialize the state variables:
+        4.1.1)Initialize the board
+            upon intialization the board should diplay the cards shuffling across the board
+            the board should have 16 squares that act as cards
+            each card will have 2 sides, one side will have the name of the game on it and the other side will have an image
+    4.1.2) Intialize the number of guesses the player has
+        the player will have 10 guesses
+    4.1.3) Intialize winner
+        -the player will win or lose based on their ability to guess all of the matches in the alloted amount of guesses
+        -if they do not get the all 8 matches they lose- a losing message will appear
+        -if the get all 8 matches a winneing message will appear
+
+
+
+5 Handle a player clicking a card
+5.2 the card should flip over when clicked
+5.1.2 the player should not be able to select more than two cards a turn
+5.1.3 if the two cards clicked match the cards remain clicked
+5.1.4 if the two cards clicked do not match the cards flip over
+5.1.5 after two cards have been clicked the number of guesses remianing is decreased by 1
+
+
+
+3.1.1 a card element that will reference each card in the game
+const cardEls = document.querySelectorAll('.card')
+3.1.2 a card back element that will reference the back of each card
+const cardBackEl = document.querySelectorAll('.back')
+3.1.3 a card face element that will reference the front of each card
+const cardFaceEl = document.querySelectorAll('.face')
+3.1.4 a counter elemnt that will refernce the number of guesses the player has after each guess?
+const guessCountDown = document.querySelector('.clickCount')
+3.1.5 a counter element that will display the number of matches
+const matchCount = document.querySelector('.matchCount')
+3.1.6  message element that will display the message when a user does or doesnt make a match
+const messageEl = document.getElementById('message')
+3.1.7 a game winner/loser message element that will display a message when the player wins or loses
+const winLoseMessage = document.getElementById('game-title')
+3.1.8 a restart button to play again
+const tryAgainEl = document.getElementById('restart-game')
--- main.js
+++ main.js
@@ -14,28 +14,22 @@ const faceArray = [
 ]
 
 
-
-
-
-
-
 /*-----  app's state variables -----*/
 
-//2.2) a winner variable
-//a variable to determine if player is a winner or loser
-let gameWinner
-//2.3) a guess variable
-let guessesLeft
+//2.1) a matches variable
+//a variable to determine the number of matches
+let matches
+//2.2) a guess variable
 //a variable that will count the number of guesses a player has
-//2.4) a variable that will reference the cards
+let guessesLeft
+//2.3) a variable that will reference the cards
 let cards = []
 //a variable that represents an array of cards to display when game starts
-//2.5) a variable that will store first click
+//2.4) a variable that will store first click
 let firstClick = ''
-//2.6 a variable that will store second click
+//2.5)a variable that will store second click
 let secondClick = ''
-let matches
-
+//2.6) Variables to store each click event
 let click1
 let click2
 
@@ -45,50 +39,32 @@ let click2
 
 
 /*----- cached elements  -----*/
-//3.1) start game button that will shuffle cards and display the cards on the board
-//const startBtnEl = document.getElementById('start-game')
-//3.1.2) a card element that will reference each card in the game
+//3.1.1) a card element that will reference each card in the game
 const cardEls = document.querySelectorAll('.card')
-// console.log(cardEls)
-//3.1.3) a card back element that will reference the back of each card
+//3.1.2) a card back element that will reference the back of each card
 const cardBackEl = document.querySelectorAll('.back')
-// console.log(cardBackEl)
-//3.1.4) a card face element that will reference the front of each card
+//3.1.3) a card face element that will reference the front of each card
 const cardFaceEl = document.querySelectorAll('.face')
-// console.log(cardFaceEl)
-//3.1.5) a counter elemnt that will refernce the number of guesses the player has after each guess?
-//you can use event.detail
-//3.1.6) a counter element that will display the number of matches
-const matchCount = document.querySelector('.matchCount')
+//3.1.4) a counter elemnt that will refernce the number of guesses the player has after each guess?
 const guessCountDown = document.querySelector('.clickCount')
-//3.1.5) wrong guess message element that will display the message when a user doesnt make a match
-//3.1.6) shuffle cards button element that will shuffle cards and restart game
-//3.1.7)guess message element that will display the message when a user has a match or doesnt have a matxh
+//3.1.5) a counter element that will display the number of matches
+const matchCount = document.querySelector('.matchCount')
+//3.1.6)  message element that will display the message when a user does or doesnt make a match
 const messageEl = document.getElementById('message')
-const tryAgainEl = document.getElementById('restart-game')
-//3.1.7) a game winner message element that will display a message when the player wins
-//3.1.8)a game loser message element that will display a message when the user loses
-//3.1.9) a game winner/loser message element that will display a message when the player wins or loses
+//3.1.7) a game winner/loser message element that will display a message when the player wins or loses
 const winLoseMessage = document.getElementById('game-title')
+//3.1.8) a restart button to play again
+const tryAgainEl = document.getElementById('restart-game')
 
 /*----- event listeners -----*/
+
+//An event listener to replay game
+//eventually this button will also shuffle the cards
 tryAgainEl.addEventListener('click', function () {
   window.location.reload()
   return
 })
 
-
-//create event listener for when a card is clicked
-
-
-
-//this should only allow you to flip two cards at a time
-//if the cards match the remain shown
-//if the cards do not match they are hidden
-
-
-//create event listener for restart game button
-//this should include an animation for shuffling the cards on screen
 
 
 
@@ -97,21 +73,23 @@ tryAgainEl.addEventListener('click', function () {
 init()
 function init() {
   board = [
-    [0, 0, 0, 0], //row
+    [0, 0, 0, 0], //row 0
     [0, 0, 0, 0], //row 1
     [0, 0, 0, 0], // row 2
-    [0, 0, 0, 0] //row 4
+    [0, 0, 0, 0] //row 3
 
   ]
 
-  matches = null
-  guessesLeft = 10
-  guessCountDown.innerText = guessesLeft
+  matches = null // there are no matches
+  guessesLeft = 10 // max number of guesses allowed
+  guessCountDown.innerText = guessesLeft// sets the counter for guesses
   cardsMatch = null
   cards = []
   render()
   addCardClickEvent(cardEls)
 }
+
+
 //render functions
 function render() {
   console.log('calling render')
@@ -120,8 +98,9 @@ function render() {
   renderCards()
 }
 //function to render board
-
 function renderCards() {
+  // this function loops through the faceArray object at the top and places an image to each card on the board
+  //Looping twice so each card has a matching image
   let cardsArray = []
   let randomCards = []
   for (let i = 0; i < faceArray.length; i++)
@@ -134,6 +113,8 @@ function renderCards() {
 
 function renderBoard() {
   // console.log('rendering board')
+  //renders the cards and ties each image to each card
+  //want to add a function so that the card images are shuffled
   let cardsArray = renderCards()
 
   cardsArray.forEach(function (img, idx) {
@@ -144,26 +125,37 @@ function renderBoard() {
 
 
 //function for clicked card
+//create event listener for when a card is clicked
+//this should only allow you to flip two cards at a time
+//if the cards match the remain shown
+//if the cards do not match they are hidden
 function addCardClickEvent(cardEls) {
 
   cardEls.forEach((card) => {
     card.addEventListener("click", (event) => {
+      // prevents user from making more than 10 guesses
       if (guessesLeft === 0) {
-        // preventDevault(event)
+        preventDevault(event)
       }
       if (firstClick === '') {
+        //if firstClick is empty then click 1 modifies the display style of card so that the image is revealed
+        //firstClick then stores the image that is revealed for comparison later
         click1 = event.target.parentElement
         firstClick = card.children[1].innerHTML
         click1.children[0].style.display = "none"
         click1.children[1].style.display = "block"
         console.log(click1)
+        //if first click is not empty (there has been a click, and second click is empty then click2 modifies the second click event object so that the face image is displayed and the back of the card is set to none)
       } else if (firstClick !== '' && secondClick === '') {
         click2 = event.target.parentElement
         secondClick = card.children[1].innerHTML
         click2.children[0].style.display = "none"
         click2.children[1].style.display = "block"
 
+        //Card match logic
         if (firstClick === secondClick) {
+          //if the cards do match the text content changes it its a match
+          //number of matches increas and the number of guesses decrease
           messageEl.textContent = "Its a match"
           console.log('its a match')
           firstClick = ''
@@ -173,11 +165,13 @@ function addCardClickEvent(cardEls) {
           guessesLeft--
           guessCountDown.innerText = guessesLeft
         if (matches === 8) {
+          //if you make 8 matches in 10 guesses or under you win
             winLoseMessage.textContent = 'YOU WIN!'
             messageEl.textContent = ''
           }
 
         } else {
+          //if you run out of guesses you loose
           if(guessesLeft === 1) {
             console.log('loser')
             guessesLeft = 0
@@ -185,8 +179,9 @@ function addCardClickEvent(cardEls) {
             winLoseMessage.textContent = 'YOU LOSE!'
             messageEl.textContent = ''}
             else{
+              //if the cards do not match the screen displays a no match message and the cards flip over
               setTimeout(() => {
-                console.log('its not a match')
+                console.log('its not a match!')
                 click1.children[0].style.display = "block"
                 click1.children[1].style.display = "none"
                 //event.target.style.display = "none";
